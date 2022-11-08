90df2958511cb914262af9d9d9ea5ab2e05185c9
Maks-Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2022-11-08 02:47:10 +0200

    deleted tick and cross

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -50,10 +50,10 @@ function createPromise(position, delay) {
 
         promise
             .then(({ position, delay }) => {
-                Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+                Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
             })
             .catch(({ position, delay }) => {
-                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+                Notify.failure(`Rejected promise ${position} in ${delay}ms`);
             });
     }
 
